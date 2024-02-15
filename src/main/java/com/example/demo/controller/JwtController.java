26ac14520d2173c3c524777e95806c383e44b1e0
rakeshkumar1436 <sanjay.rakesh.gupta@gmail.com>
2024-02-16 01:57:39 +0530

    new file add

--- src/main/java/com/example/demo/controller/JwtController.java
+++ src/main/java/com/example/demo/controller/JwtController.java
@@ -1,10 +1,48 @@
 package com.example.demo.controller;
 
+import com.example.demo.helper.JwtHelper;
+import com.example.demo.model.JwtRequest;
+import com.example.demo.model.JwtResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.CachingUserDetailsService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class JwtController {
-    @r
-    public ResponseEntity<?>generateToken(){
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private CachingUserDetailsService cachingUserDetailsService;
+    @Autowired
+    private JwtHelper jwtHelper;
 
-    }
-}
+    @RequestMapping(value = "/token", method = RequestMethod.POST)
+    public ResponseEntity<?> generateToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+        System.out.println(jwtRequest);
+
+        try {
+            this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(), jwtRequest.getPassword()));
+
+
+        } catch (UsernameNotFoundException e) {
+            e.printStackTrace();
+            throw new Exception("bad credenting");
+
+        }
+
+        UserDetails userDetails = this.cachingUserDetailsService.loadUserByUsername(jwtRequest.getUsername());
+        String token = this.jwtHelper.generateToken(userDetails);
+        System.out.println("jwt" + token);
+        return ResponseEntity.ok(new JwtResponse(token));
+
+
+
+    }}
