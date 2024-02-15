b09dca14e24ba3e38901e73e8d2af69b55cfbe7f
rakeshkumar1436 <sanjay.rakesh.gupta@gmail.com>
2024-02-15 20:45:06 +0530

    controller create

---
+++ src/main/java/com/example/demo/controller/JwtController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+
+public class JwtController {
+    @r
+    public ResponseEntity<?>generateToken(){
+
+    }
+}
